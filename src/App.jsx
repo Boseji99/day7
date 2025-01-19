a7216c655d72654b263b3a2bd1e17138e45cdebb
Sumon Bose <boseji99@gamil.com>
2025-01-19 19:09:28 +0530

    modified:   src/App.jsx 	new file:   src/utility/data.js 	new file:   src/utility/script.js

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,24 @@
 import React from 'react'
+import productdata from './utility/data';
 
 const App = () => {
+  let previews = productdata.map((product) => {
+    let reviews = product.productReviews;
+    let productReviews = reviews.map((review) => review.productReview);
+    return { productName: product.productName, productReviews };
+});
   return (
-    <div>
+    <div className="bg-slate-700 text-white text-center">
+    product views
+      {previews.map((product) => (
+        <div>
       
+        <p>{product.productName}</p>
+        <p>{product.productReviews}</p>
+
+        </div>
+        
+    ))}
     </div>
   )
 }
---
+++ src/utility/script.js
@@ -0,0 +1,4 @@
+import productdata from "./data.js";
+console.log(productdata);
+
+console.log(pr);
